ab46139348ada243e62c645e64f60a850aa6f02b
giselakayy <giselakottmeier@gmail.com>
2016-02-06 15:34:38 -0800

    Upon user requested recipes, DB is making a request to SuperMarker API to fetch prices per recipe item.

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -4,6 +4,8 @@ var Promise = require('bluebird');
 var request = require('request');
 var apiKeys = require('../config/apiKeys');
 var priceController = require('./priceController');
+var parseString = require('xml2js').parseString;
+
 
 var client = new pg.Client(connectionString);
 
@@ -12,7 +14,8 @@ var cooking = {
 	2: 1800, // hour in secs
 	3: 3600 // two hour in secs
 }
-
+var pageNumber = 1;
+var pickNumber = 1;
 	// getRecipesFromEdaman = function (){
 	// 	request("https://api.edamam.com/search?&&app_id=" + apiKeys.edamam.id + "&app_key=" + apiKeys.edamam.key + "", function (err, data){
 	// 		console.log(JSON.parse(data.body).hits)
@@ -26,9 +29,7 @@ var cooking = {
 		console.log("in yummly",uid)
 
 		var foodQ = function (){
-			console.log("running foodQ")
 			return new Promise (function (resolve, reject) {
-				console.log("returning foodq promise")
 				var start = 0;
 				var startQuery = client.query("SELECT Count(*) FROM recipes WHERE sourceid=1", function (err, result){
 					if (result) {
@@ -38,7 +39,24 @@ var cooking = {
 
 				})
 				startQuery.on("end", function (){
-					var userCookingTime = client.query("SELECT cookingTime from Profiles WHERE id = '" + uid + "'", function (err, data){
+					// var userCookingTime = client.query("SELECT cookingTime from Profiles WHERE id = '" + uid + "'", function (err, data) {
+						client.query('SELECT name from RecipeSearchTerms WHERE id = ' + pickNumber + ' ', function (err, result) {
+							var foodName = result.rows[0].name;
+								// console.log("foeaching:", row)
+								request("https://api.edamam.com/search?q=" + foodName + "&from=10&to=20&app_id=21198cff&app_key=a70d395eb9f3cf9dae36fb4b5e638958", function (err, response, body) {
+									if (err) {console.log('Error in request to edemam', err);} 
+									else {
+										resolve(JSON.parse(response.body).hits)
+									}
+									// console.log(JSON.parse(response.body).hits)
+									// console.log('https://api.edamam.com/search?q=' + row.name + '&app_id=21198cff&app_key=a70d395eb9f3cf9dae36fb4b5e638958')
+								// })
+							});
+							// console.log("foodname:", foodName)
+						// })
+						// pick x search terms
+						// for eac
+						// ≈
 						// request("http://api.yummly.com/v1/api/recipes?_app_id=" + apiKeys.yummly.id +
 						// "&_app_key=" + apiKeys.yummly.key +
 						// "&requirePictures=true" +
@@ -54,58 +72,82 @@ var cooking = {
 						// 	if (!error && response.statusCode == 200) {
 						// 		yummlyRecipes = body
 						// 		// console.log("result:", yummlyRecipes)
-						// 		resolve(yummlyRecipes);
+						// res.end()
+								// resolve(yummlyRecipes);
 						// 	}
 						// })
 
+
+
 					});
-				})
-			})
+				});
+			});
 		}
 
 		foodQ().then(function (yummlyRecipes) {
-			// console.log("about to insert into db")
-			var addIngriedientToDB = function (ingredientLine) {
-				// console.log(ingredientLine)
-				var addIngredientsQuery = client.query("INSERT INTO ingredients (name) VALUES (" + ingredientLine +")", function () {
-					//parse here
-					priceController.findPrice(ingredientLine);
+			console.log("about to insert into db")
+			var addIngriedientToDB = function (item) {
+				console.log("AddIngredientoDB:", item);
+				request("http://www.SupermarketAPI.com/api.asmx/COMMERCIAL_SearchByProductName?APIKEY=APIKEY&ItemName=" + item.food + "", function (err, response, xml) {
+					parseString(xml, function (err, result) {
+						if (err) {
+							console.log("Parsing XML Error")
+						}
+						else {
+							// Pick a better one than the first
+							var productList = result.ArrayOfProduct_Commercial.Product_Commercial;
+							var index = productList.length >= 5 ? 5 : productList.length-1;
+							var choice = productList[index];
+							console.log("productList:", productList, "index:", index, "choice", choice);
+							client.query("INSERT INTO GroceryPrices (name, description, price) VALUES ('"+ choice.Itemname + "','" + choice.ItemDescription + "'," + choice.Pricing[0] + ");", function(err, productData) {
+								if (err) {
+									console.log("Error in inserting to GroceryPrices:", err);
+								} else {
+									console.log("GroceryPrices result:", productData);
+								}
+							})
+						}
+					    // console.log("LOOK AT THISS: ", );
+					});
 				})
-			}
-			var getIngredientsFromYummly = function (recipeid, recipe) {
-				request("http://api.yummly.com/v1/api/recipe/" + recipeid + "?_app_id=" + apiKeys.yummly.id + "&_app_key=" + apiKeys.yummly.key + "", function (err, result) {
-					JSON.parse(result.body).ingredientLines.forEach( function (ingriedient) {
-				console.log("I Source:",recipe.sourceDisplayName, "ingriedient: ", ingriedient)
-						addIngriedientToDB(ingriedient);
-					})
-
-				})
-			}
+				// var addIngredientsQuery = client.query("INSERT INTO ingredients (name, measure, quantity, description) VALUES ('" + item.food + "','" + item.measure + "'," + item.quantity + ",'" + item.text + "')", function (err, data) {
+				// 	if (err) { console.log("ERROR IN INSERT INGREDIENTS:", err)}
+				// 	else { console.log("NO ERROR:", data)}
+				// 	// priceController.findPrice(item);
+				// })
+			};
 			var insertRecipesIntoDB = function (recipe) {
-				var addCookingTime = function (){
-					if (recipe.totalTimeInSeconds >= cooking[2]) {
-						return cookingTime = 3;
-					} else if (recipe.totalTimeInSeconds >= cooking[1] && recipe.totalTimeInSeconds < cooking[2]) {
-						return cookingTime = 2;
-					} else {
-						return cookingTime = 1;
-					} 	
-				}
-				console.log("da recipe:", recipe)
-				var recipeImg = recipe.smallImageUrls ? recipe.smallImageUrls[0] + "0-c": recipe.imageUrlsBySize[Object.keys(recipe.imageUrlsBySize)[0]].replace("90","900")
-				client.query("INSERT INTO Recipes (name, exactcookingtime, image, directionsUrl, cookingtime, recipesourceid, rating, sourceid) VALUES ('" + recipe.recipeName + "', " + recipe.totalTimeInSeconds + ", '" + recipeImg + "0-c', 'http://www.yummly.com/recipe/external/" + recipe.id + "', " + addCookingTime() + ", '" + recipe.id + "', " + recipe.rating + ", 1) " , function (err) {
+				console.log("RECIPE ingredients:", recipe.ingredients);
+
+				// var addCookingTime = function (){
+				// 	if (recipe.totalTimeInSeconds >= cooking[2]) {
+				// 		return cookingTime = 3;
+				// 	} else if (recipe.totalTimeInSeconds >= cooking[1] && recipe.totalTimeInSeconds < cooking[2]) {
+				// 		return cookingTime = 2;
+				// 	} else {
+				// 		return cookingTime = 1;
+				// 	} 	
+				// }
+				// console.log("da recipe:", recipe)
+				// var recipeImg = recipe.smallImageUrls ? recipe.smallImageUrls[0] + "0-c": recipe.imageUrlsBySize[Object.keys(recipe.imageUrlsBySize)[0]].replace("90","900")
+				console.log(recipe.label, recipe.image, recipe.url, 2 );
+				client.query("INSERT INTO Recipes (name, image, directionsUrl, sourceid) VALUES ('" + recipe.label + "', '" + recipe.image + "', '" + recipe.url + "'," + 2 + ")", function (err) {
 					if (err){
-						console.log("yummly recipe already saved in db")
+						console.log("Edamam recipe already saved in db", err)
+					} else {
+						console.log("added recipe to database")
 					}
-					console.log("added recipe to database")
+				})
+				recipe.ingredients.forEach(function(ingredient) {
+					addIngriedientToDB(ingredient);
 				})
 
 			};
-			yummlyRecipes = JSON.parse(yummlyRecipes);
-			yummlyRecipes.matches.forEach(function (recipe, index) {
-
-				getIngredientsFromYummly(recipe.id, recipe)
-				insertRecipesIntoDB(recipe);
+			console.log(yummlyRecipes)
+			// yummlyRecipes = JSON.parse(yummlyRecipes);
+			yummlyRecipes.forEach(function (item) {
+				// getIngredientsFromYummly(item.recipe)
+				insertRecipesIntoDB(item.recipe);
 			})
 		});
 	}
@@ -162,7 +204,7 @@ module.exports = {
 				var sendData = {recipes: recipeResults }
 				// console.log("sending this thingy:",sendData)	
 				res.status(200).json(sendData);
-				var lowOnViableRecipes = 500;
+				var lowOnViableRecipes = 1000;
 				console.log("getting from yummly")
 				if (totalMatches < lowOnViableRecipes) {
 					client.end();
